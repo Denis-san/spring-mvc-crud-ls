0ec5d1bcc87c95002e0a8128231aee7f3e212722
Denis-san <denilson.nt.nc@gmail.com>
2022-03-13 11:27:19 -0300

    project created

---
+++ src/main/java/br/com/san/ls/SpringMvcCrudLsApplication.java
@@ -0,0 +1,13 @@
+package br.com.san.ls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCrudLsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcCrudLsApplication.class, args);
+	}
+
+}
